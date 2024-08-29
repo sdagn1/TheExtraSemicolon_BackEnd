f3e71c8bb804d4507b79a3c721c4b1fb4a47fe91
Dylan Slater <dylan.slater@kainos.com>
2024-08-29 09:25:29 +0100

    added linter passing code

--- src/main/java/org/example/TestApplication.java
+++ src/main/java/org/example/TestApplication.java
@@ -35,8 +35,11 @@ public class TestApplication extends Application<TestConfiguration> {
                     final Environment environment) {
         environment.jersey()
                 .register(new TestController(new TestService(new TestDao())));
-        environment.jersey().register(new JobRoleController(new JobRoleService(new JobRoleDao())));
-        String test = "this is a test string to break linter so that the checks in github fail... here goes" + "allow it man";
+        environment
+                .jersey()
+                .register(new JobRoleController(
+                        new JobRoleService(
+                                new JobRoleDao())));
     }
 
 }
