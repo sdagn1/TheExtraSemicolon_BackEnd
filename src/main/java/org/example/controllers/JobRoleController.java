499d33a3d52626afccb2736a155bd2000a3a4a7b
Sam Dagnall <sam.dagnall@kainos.com>
2024-09-03 14:35:16 +0100

    Added Authorization on the back-end for JobRoles

--- src/main/java/org/example/controllers/JobRoleController.java
+++ src/main/java/org/example/controllers/JobRoleController.java
@@ -1,15 +1,21 @@
 package org.example.controllers;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.example.exceptions.DoesNotExistException;
+import org.example.models.UserRole;
 import org.example.services.JobRoleService;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
+import java.util.List;
 
 @Api("job-roles")
 @Path("/api/job-roles")
@@ -23,6 +29,12 @@ public class JobRoleController {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({UserRole.ADMIN, UserRole.USER})
+    @ApiOperation(
+            value = "Returns a List of Job Role Responses",
+            authorizations = @Authorization(value = HttpHeaders.AUTHORIZATION),
+            response = List.class
+    )
     public Response getJobRoles() throws SQLException {
         try {
             return Response.ok()
