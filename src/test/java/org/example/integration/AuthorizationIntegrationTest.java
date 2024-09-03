207309a02e1b339e25e14e3897cb30040cd4135d
Sam Dagnall <sam.dagnall@kainos.com>
2024-09-03 15:13:54 +0100

    Added authentication to JobRoles Integration test and added AuthorizationIntegrationTest

--- src/test/java/org/example/integration/AuthorizationIntegrationTest.java
+++ src/test/java/org/example/integration/AuthorizationIntegrationTest.java
@@ -5,7 +5,6 @@ import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.example.TestApplication;
 import org.example.TestConfiguration;
-import org.example.models.JobRoleResponse;
 import org.example.models.LoginRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,41 +14,25 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
-public class JobRolesIntegrationTest {
+public class AuthorizationIntegrationTest {
     static final DropwizardAppExtension<TestConfiguration> APP = new DropwizardAppExtension<>(
             TestApplication.class, null,
             new ResourceConfigurationSourceProvider()
     );
 
     @Test
-    void getAllJobRoles_shouldReturnAllJobRoles() {
+    void getAllJobRoles_shouldReturn401Error_whenUnauthorized() {
 
         Client client = APP.client();
-
-        LoginRequest loginRequest = new LoginRequest(
-                "admin@kainos.com",
-                "wlSNgEn5dCBM59jnbeH+txKWn36Vt6QScELcAa5ZBNduqSY16JAl2hqeGsZrmpG0kdb9+ILMoCJVB3er8ZoCJI9o26IM83UfnJtTT3p7cRgOUxsU0iMHgkI9KdQpDim6"
-
-        );
-        String token = "Bearer " + client
-                .target("http://localhost:8080/api/auth/login")
-                .request()
-                .post(Entity.json(loginRequest))
-                .readEntity(String.class);
 
         Response response = client
                 .target("http://localhost:8080/api/job-roles")
                 .request()
-                .header(HttpHeaders.AUTHORIZATION, token)
                 .get();
 
-        Assertions.assertEquals(200, response.getStatus());
+        Assertions.assertEquals(401, response.getStatus());
 
     }
-
 }
