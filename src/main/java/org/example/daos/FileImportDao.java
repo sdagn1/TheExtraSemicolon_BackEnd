cb5e97908c0386459265500e40fcf09a28039fc4
jemorak <jemima.orakwue@hotmail.co.uk>
2024-09-05 14:51:31 +0100

    SQL Query works

--- src/main/java/org/example/daos/FileImportDao.java
+++ src/main/java/org/example/daos/FileImportDao.java
@@ -1,4 +1,5 @@
 package org.example.daos;
+import javax.xml.transform.Result;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 
@@ -6,49 +7,87 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 
 public class FileImportDao {
 
-    public int importRoles() throws SQLException {
+    public int importRoles(final List<String> listOfLines) throws SQLException {
         try (Connection connection = DatabaseConnector.getConnection()) {
             String insertStatement = "INSERT INTO Job_Roles"
                     + "(roleName, description, responsibilities, "
                     + "linkToJobSpec, capability, band, closingDate, `status`, "
                     + "positionsAvailable, locations)"
                     + "VALUES"
-                    + "(?,?,?,?,?,?,?,?,?,?;) ";
+                    + "(?,?,?,?,?,?,?,?,?,?);";
 
             PreparedStatement statement = connection.prepareStatement(
                     insertStatement,
                     PreparedStatement.RETURN_GENERATED_KEYS);
+            String[] lineContents = new String[listOfLines.size()];
+            for (String line : listOfLines) {
+                lineContents = line.split(",");
+            }
+            System.out.println("Number 0" + lineContents[0]);
+            System.out.println("Number 1" + lineContents[1]);
+            System.out.println("Number 2" + lineContents[2]);
+            System.out.println("Number 3" + lineContents[3]);
+            System.out.println("Number 4" + lineContents[4]);
+            System.out.println("Number 5" + lineContents[5]);
+            System.out.println("Number 6" +lineContents[6]);
+            System.out.println("Number 7" +lineContents[7]);
+            System.out.println("Number 8" +lineContents[8]);
+            System.out.println("Number 9" +lineContents[9]);
+            System.out.println("Number 10" +lineContents[10]);
+            System.out.println("Number 11" +lineContents[11]);
+
+
+            statement.setString(1, lineContents[0]);
+            statement.setString(2, lineContents[1]);
+            statement.setString(3, lineContents[2]);
+            statement.setString(4, lineContents[5]);
+            statement.setString(5, lineContents[6]);
+            statement.setInt(6, Integer.parseInt(lineContents[7]));
+            statement.setString(7, lineContents[8]);
+            statement.setString(8, lineContents[9]);
+            statement.setInt(9, Integer.parseInt(lineContents[10]));
+            statement.setString(10, lineContents[11]);
+
+            int resultSet = statement.executeUpdate();
+
+            System.out.println(resultSet);
+
+
 
 //          InputStreamReader sr = new InputStreamReader((new FileInputStream(
 //                  "/Users/jemima.orakwue/Documents/test.csv")));
 //          BufferedReader br = new BufferedReader(new
 //                  FileReader(
 //                  "/Users/jemima.orakwue/Documents/test.csv"));
-
-            FileInputStream fileInputStream = new FileInputStream(
-                    "/Users/jemima.orakwue/Documents/test.csv");
-            InputStreamReader inputStreamReader = new InputStreamReader(
-                    fileInputStream);
-            BufferedReader bufferedReader =
-                    new BufferedReader(inputStreamReader);
-
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                // Process the line
-                System.out.println(line);
-            }
-
-            bufferedReader.close();
-            inputStreamReader.close();
-            fileInputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+//
+//            FileInputStream fileInputStream = new FileInputStream(
+//                    "/Users/jemima.orakwue/Documents/test.csv");
+//            InputStreamReader inputStreamReader = new InputStreamReader(
+//                    fileInputStream);
+//            BufferedReader bufferedReader =
+//                    new BufferedReader(inputStreamReader);
+//
+//            String line;
+//            while ((line = bufferedReader.readLine()) != null) {
+//                // Process the line
+//                System.out.println(line);
+//            }
+//
+//            bufferedReader.close();
+//            inputStreamReader.close();
+//            fileInputStream.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+            return 0;
         }
-        return 0;
     }
 }
+
--- src/main/java/org/example/services/BucketService.java
+++ src/main/java/org/example/services/BucketService.java
@@ -13,13 +13,15 @@ import org.example.daos.DatabaseConnector;
 import org.example.daos.FileImportDao;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BucketService {
-    //FileImportDao fileImportDao;
+    FileImportDao fileImportDao = new FileImportDao();
     DatabaseConnector databaseConnector;
 
     public BucketService() {
@@ -35,12 +37,6 @@ public class BucketService {
                 .build();
 
         String filename = "test.csv";
-//        ListObjectsV2Result result = s3client.listObjectsV2(System.getenv().
-//        get("S3_BUCKET"));
-//        List<S3ObjectSummary> objects = result.getObjectSummaries();
-//        for (S3ObjectSummary os : objects) {
-//            System.out.println("* " + os.getKey());
-//        }
 
         System.out.format("Downloading %s from S3 bucket %s...\n",
                 filename, System.getenv().get("S3_BUCKET"));
@@ -49,22 +45,24 @@ public class BucketService {
             S3Object o = s3client.getObject(System.getenv().get(
                     "S3_BUCKET"), filename);
             S3ObjectInputStream s3is = o.getObjectContent();
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(s3is));
-
+            InputStreamReader inputStreamReader = new InputStreamReader(
+                    s3is);
+            BufferedReader bufferedReader = new BufferedReader(
+                    inputStreamReader);
+            System.out.println("AWS FILE HERE");
             String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+            List<String> listOfLines = new ArrayList<>();
+            while ((line = bufferedReader.readLine()) != null) {
+                // Process the line
+//                System.out.println(line);
+                listOfLines.add(line);
             }
+//            System.out.println(listOfLines);
+            fileImportDao.importRoles(listOfLines);
 
-//            FileOutputStream fos = new FileOutputStream(new File(filename));
-//            byte[] read_buf = new byte[1024];
-//            int read_len = 0;
-//            while ((read_len = s3is.read(read_buf)) > 0) {
-//                fos.write(read_buf, 0, read_len);
-//            }
-            s3is.close();
-            reader.close();
+
+            bufferedReader.close();
+            inputStreamReader.close();
         } catch (AmazonServiceException e) {
             System.err.println(e.getErrorMessage());
             System.exit(1);
@@ -74,30 +72,8 @@ public class BucketService {
         } catch (IOException e) {
             System.err.println(e.getMessage());
             System.exit(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-
-        try {
-            FileInputStream fileInputStream = new FileInputStream(
-                    "/Users/jemima.orakwue/Documents/test.csv");
-            InputStreamReader inputStreamReader = new InputStreamReader(
-                    fileInputStream);
-            BufferedReader bufferedReader =
-                    new BufferedReader(inputStreamReader);
-
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-
-
-                System.out.println(line);
-            }
-
-            bufferedReader.close();
-            inputStreamReader.close();
-            fileInputStream.close();
-        } catch (
-                IOException e) {
-            e.printStackTrace();
-        }
-
     }
 }
