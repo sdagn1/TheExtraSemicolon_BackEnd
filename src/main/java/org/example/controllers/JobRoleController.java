1ce6c20f56df3e6caabb73d8aee1c3190b83c4f1
sdagn1 <77390325+sdagn1@users.noreply.github.com>
2024-09-06 08:35:59 +0100

    009 authorisation system (#15)
    * Added UserRole class
    
    * Added JwtToken models, and Authorisers
    
    * Added Authorization on the back-end for JobRoles
    
    * Added authentication to JobRoles Integration test and added AuthorizationIntegrationTest
    
    * Added more integration tests
    
    * Added Authorization to Unit Tests

--- src/main/java/org/example/controllers/JobRoleController.java
+++ src/main/java/org/example/controllers/JobRoleController.java
@@ -1,16 +1,22 @@
 package org.example.controllers;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.example.exceptions.DoesNotExistException;
+import org.example.models.UserRole;
 import org.example.services.JobRoleService;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
+import java.util.List;
 
 @Api("job-roles")
 @Path("/api/job-roles")
@@ -24,6 +30,12 @@ public class JobRoleController {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({UserRole.ADMIN, UserRole.USER})
+    @ApiOperation(
+            value = "Returns a Job Role",
+            authorizations = @Authorization(value = HttpHeaders.AUTHORIZATION),
+            response = List.class
+    )
     public Response getJobRoleById(final @PathParam("id") int id) {
         try {
             return Response.ok().entity(jobRoleService.getJobRoleById(id))
@@ -38,6 +50,12 @@ public class JobRoleController {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({UserRole.ADMIN, UserRole.USER})
+    @ApiOperation(
+            value = "Returns a List of Job Role Responses",
+            authorizations = @Authorization(value = HttpHeaders.AUTHORIZATION),
+            response = List.class
+    )
     public Response getJobRoles() {
         try {
             return Response.ok()
